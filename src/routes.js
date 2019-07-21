e0849a4bab8c380f754c71aaf6781299b20e6a7d
Dmitriy Minaev <minaevdup@gmail.com>
2019-07-21 18:44:02 +0300

    feat(app): added not found page

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import { renderRoutes } from 'react-router-config';
 
 import { CreateLinkRoutes } from 'features/create-link';
+const NotFoundPage = lazy(() =>
+  import('features/common').then(a => ({ default: a.NotFoundPage }))
+);
 
-const createRoutes = [...CreateLinkRoutes(), { components: <></> }];
+const createRoutes = [...CreateLinkRoutes(), { component: NotFoundPage }];
 
 export const Routes = () => <>{renderRoutes(createRoutes)}</>;
