510464093d6282857fc50d053abd40236e143220
Dmitriy Minaev <minaevdup@gmail.com>
2019-07-24 21:37:12 +0300

    feat(app): font connection

--- src/ui/templates/global-styles.js
+++ src/ui/templates/global-styles.js
@@ -1,8 +1,24 @@
 import { createGlobalStyle } from 'styled-components';
 
+const fontFace = (name, src, fontWeight = 'normal', fontStyle = 'normal') => `
+	@font-face{
+		font-family: "${name}";
+		src: url(${require(`../../../public/fonts/${src}.ttf`)})  format("truetype");
+
+		font-style: ${fontStyle};
+		font-weight: ${fontWeight};
+	}
+`;
+
 export const GlobalStyle = createGlobalStyle`
+	${fontFace('Open Sans', 'OpenSans-Light', 300)};
+	${fontFace('Open Sans', 'OpenSans-Regular', 400)};
+	${fontFace('Open Sans', 'OpenSans-Bold', 700)};
+
 	body {
 		margin: 0;
+		font-family: 'Open Sans', sans-serif;
+		font-weight: 700;
 	};
 
 	#root {
