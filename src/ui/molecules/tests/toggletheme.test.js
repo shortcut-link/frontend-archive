3c80c7fd9c0b2b25601c130bf3a5dd7af3537ffc
Dmitriy Minaev <minaevdup@gmail.com>
2019-07-25 21:10:48 +0300

    test(ui/molecules): toggle select theme, renamed to templates

---
+++ src/ui/molecules/tests/toggletheme.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+import { mount } from 'enzyme';
+import { ThemeProvider } from 'styled-components';
+
+import { ToggleSelectTheme } from '../toggletheme';
+
+/* Используется ThemeProvider,
+ * так как в компоненте берётся цвет из темы
+ */
+
+describe('should render Toggle Select Theme', () => {
+  let theme = { palette: { primary: { initial: { background: '#fff' } } } };
+
+  const providerToggle = props => (
+    <ThemeProvider theme={theme}>
+      <ToggleSelectTheme {...props} />
+    </ThemeProvider>
+  );
+
+  it('dark theme is disabled', () => {
+    const tree = renderer
+      .create(providerToggle({ isDark: false, toggle: () => {} }))
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('dark theme is on', () => {
+    const tree = renderer
+      .create(providerToggle({ isDark: true, toggle: () => {} }))
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('click to change themes', () => {
+    const mockFunction = jest.fn();
+
+    const component = mount(
+      providerToggle({ isDark: false, toggle: mockFunction })
+    );
+
+    component.find('input[type="checkbox"]').simulate('click');
+
+    expect(mockFunction).toHaveBeenCalled();
+
+    component.unmount();
+  });
+});
