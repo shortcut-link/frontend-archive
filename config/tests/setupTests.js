94e0ed72e8afcd88a38cd67047c066bec18efef6
Dmitriy Minaev <minaevdup@gmail.com>
2019-08-31 18:31:14 +0300

    test(api/link): create short link, add jest-fetch-mock, commitizen

--- config/tests/setupTests.js
+++ config/tests/setupTests.js
@@ -3,3 +3,5 @@ import Adapter from 'enzyme-adapter-react-16';
 import 'jest-styled-components';
 
 configure({ adapter: new Adapter() });
+
+global.fetch = require('jest-fetch-mock');
---
+++ src/api/tests/link.test.js
@@ -0,0 +1,37 @@
+import { linkAPI } from '../link';
+import { switchError } from '../link.errors';
+
+describe('testing api links', () => {
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
+
+  const link = 'example.com';
+  const options = { headers: { 'Content-Type': 'application/json ' } };
+
+  it('should shorten the link', () => {
+    const shortUrl = 'http://localhost:8080/qwerty';
+
+    fetch.mockResponse(
+      JSON.stringify({ ok: true, result: { url: shortUrl } }),
+      options
+    );
+
+    linkAPI.createLink(link).then(({ url }) => {
+      expect(url).toEqual(shortUrl);
+    });
+
+    expect(fetch.mock.calls.length).toEqual(1);
+    expect(fetch.mock.calls[0][0].url).toEqual('/api/link');
+  });
+
+  it('should return an unknown error', () => {
+    fetch.mockResponse(JSON.stringify({ ok: false, error: 'unkown' }), {
+      options
+    });
+
+    linkAPI.createLink(link).catch(error => {
+      expect(error).toEqual(switchError(error));
+    });
+  });
+});
