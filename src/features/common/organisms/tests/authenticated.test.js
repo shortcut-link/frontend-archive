952ad34522f1c96809fef595930f56573eaf49fc
Dmitriy Minaev <minaevdup@gmail.com>
2019-09-02 12:13:17 +0300

    test(features/common): authenticated

---
+++ src/features/common/organisms/tests/authenticated.test.js
@@ -0,0 +1,27 @@
+import '../../model/session.model';
+import React from 'react';
+import { create, act } from 'react-test-renderer';
+
+import { Authenticated } from '../authenticated';
+import { sessionChange } from '../../model/session.events';
+
+describe('must render Header', () => {
+  const renderExists = () => <div>renderExists</div>;
+  const renderEmpty = () => <div>renderEmpty</div>;
+
+  const component = create(
+    <Authenticated renderExists={renderExists} renderEmpty={renderEmpty} />
+  );
+
+  it('without session', () => {
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('with session', () => {
+    act(() => {
+      sessionChange({ email: 'example.com' });
+    });
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
