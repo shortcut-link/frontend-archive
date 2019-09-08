e2e3a98aa3676e0045e971862481e2df35e6f292
Dmitriy Minaev <minaevdup@gmail.com>
2019-09-08 10:55:02 +0300

    refactor(features/profile): remove return for functional function

--- src/features/profile/atom/card.js
+++ src/features/profile/atom/card.js
@@ -4,16 +4,14 @@ import styled from 'styled-components';
 import { Card } from 'ui';
 import { Col } from 'lib/styled-components';
 
-export const CardProfile = ({ heading, children }) => {
-  return (
-    <Card>
-      <Col>
-        <Heading>{heading}</Heading>
-        {children}
-      </Col>
-    </Card>
-  );
-};
+export const CardProfile = ({ heading, children }) => (
+  <Card>
+    <Col>
+      <Heading>{heading}</Heading>
+      {children}
+    </Col>
+  </Card>
+);
 
 const Heading = styled.h4`
   font-size: 1.2rem;
