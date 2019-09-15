44255f75d797912161fa9d0cc5a34ee0f253922a
Dmitriy Minaev <minaevdup@gmail.com>
2019-09-15 12:20:39 +0300

    fix(ui/molecules): the toggle did not stretch, the excess container

--- src/ui/molecules/toggle-text.js
+++ src/ui/molecules/toggle-text.js
@@ -2,25 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { Toggle } from '../atom';
-import { Row, Col } from 'lib/styled-components';
+import { Row } from 'lib/styled-components';
 
 export const ToggleWithText = ({ text, id, toggle, defaultChecked }) => {
   return (
-    <ColContainer gap="1rem">
-      <Row gap="0.8rem" align="center">
-        <Label htmlFor={id}>{text}</Label>
-        <Toggle toggle={toggle} id={id} defaultChecked={defaultChecked} />
-      </Row>
-    </ColContainer>
+    <Row gap="0.8rem" align="center" justify="space-between">
+      <Label htmlFor={id}>{text}</Label>
+      <Toggle toggle={toggle} id={id} defaultChecked={defaultChecked} />
+    </Row>
   );
 };
-
-const ColContainer = styled(Col)`
-  & > :not(:last-child) {
-    border-bottom: 1px solid ${({ theme }) => theme.palette.decoration.borders};
-    padding-bottom: 1rem;
-  }
-`;
 
 const Label = styled.label`
   cursor: pointer;
