8e70cf2a01f57bfc5c7f7fb1cf163fa42f046315
Dmitriy Minaev <minaevdup@gmail.com>
2019-10-29 16:32:25 +0300

    refactor(features/profile): add autosizer in table

--- src/features/profile/templates/links-table.tsx
+++ src/features/profile/templates/links-table.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useStore } from 'effector-react';
-import { Event } from 'effector';
-import { InfiniteLoader, Table, Column } from 'react-virtualized';
+import { InfiniteLoader, Table, Column, AutoSizer } from 'react-virtualized';
+import styled from 'styled-components';
 import 'react-virtualized/styles.css';
 
 import { ItemLinksTable } from '../atom';
@@ -14,7 +14,7 @@ import {
 } from 'pages/profile/model/links';
 
 interface LinksTableProps {
-  openLinkManagement: Event<number>;
+  openLinkManagement: (payload: number) => void;
 }
 
 export const LinksTable: React.FC<LinksTableProps> = ({
@@ -32,33 +32,56 @@ export const LinksTable: React.FC<LinksTableProps> = ({
   }, []);
 
   return (
-    <InfiniteLoader
-      isRowLoaded={({ index }) => links[index]}
-      loadMoreRows={downloadLinksProcessing}
-      rowCount={countUserLinks}
-      tabIndex={2}
-    >
-      {({ onRowsRendered, registerChild }) => (
-        <Table
-          ref={registerChild}
-          width={210 + 250 + 150 + 150}
-          height={300}
-          headerHeight={20}
-          rowHeight={35}
-          rowCount={links.length}
-          onRowsRendered={onRowsRendered}
-          rowGetter={({ index }) => links[index]}
-          rowRenderer={ItemLinksTable}
-          onRowClick={({ index }) => openLinkManagement(index)}
-          tabIndex={3}
-          aria-label="Your links"
-        >
-          <Column label="Url" dataKey="url" width={210} />
-          <Column label="Original Url" dataKey="originalUrl" width={250} />
-          <Column label="Transitions" dataKey="transitions" width={150} />
-          <Column label="Created at" dataKey="createdAt" width={150} />
-        </Table>
-      )}
-    </InfiniteLoader>
+    <Container>
+      <InfiniteLoader
+        isRowLoaded={({ index }) => links[index]}
+        loadMoreRows={downloadLinksProcessing}
+        rowCount={countUserLinks}
+        tabIndex={2}
+      >
+        {({ onRowsRendered, registerChild }) => (
+          <AutoSizer>
+            {({ width, height }) => (
+              <Table
+                ref={registerChild}
+                width={width}
+                height={height}
+                onRowsRendered={onRowsRendered}
+                rowRenderer={ItemLinksTable}
+                tabIndex={3}
+                headerHeight={20}
+                rowHeight={35}
+                rowCount={links.length}
+                aria-label="Your links"
+                rowGetter={({ index }) => links[index]}
+                onRowClick={({ index }) => openLinkManagement(index)}
+              >
+                <Column label="Url" dataKey="url" width={width * 0.6} />
+                <Column
+                  label="Original Url"
+                  dataKey="originalUrl"
+                  width={width * 0.7}
+                />
+                <Column
+                  label="Transitions"
+                  dataKey="transitions"
+                  width={width * 0.4}
+                />
+                <Column
+                  label="Created at"
+                  dataKey="createdAt"
+                  width={width * 0.4}
+                />
+              </Table>
+            )}
+          </AutoSizer>
+        )}
+      </InfiniteLoader>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  height: 400px;
+  width: 750px;
+`;
