66dbd7bce77ce638f388d80a36ebed04ba5babd5
Dmitriy <minaevdup@gmail.com>
2019-11-12 13:48:32 +0300

    feat(api/link): link search error handling

--- src/api/link.errors.ts
+++ src/api/link.errors.ts
@@ -4,6 +4,9 @@ export const linkError = ({ message }: ErrorResponse) => {
   let textError;
 
   switch (message) {
+    case 'link_not_found':
+      textError = 'Link with this URL not found';
+      break;
     default:
       textError = 'Got an unexpected error. Try again later';
   }
