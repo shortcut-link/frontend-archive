87582c22b0d116e01405f7cfff8a76cff79bea28
Dmitriy Minaev <minaevdup@gmail.com>
2019-11-16 20:05:15 +0300

    build(app): upgrade react, react-scripts, cross-env, typescript

--- src/ui/templates/index.ts
+++ src/ui/templates/index.ts
@@ -1,7 +1,6 @@
-import { IHeaderAndFooter } from './main';
-export type IHeaderAndFooter = IHeaderAndFooter;
-
 export { Main } from './main';
 export { Container } from './container';
 export { GlobalStyle } from './global-styles';
 export { CenterContent } from './center-content';
+
+export type IHeaderAndFooter = import('./main').IHeaderAndFooter;
